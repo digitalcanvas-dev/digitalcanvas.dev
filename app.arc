a8fcf63a0d8e5386b95256802cd99a31fb8351b5
goldins <simon.goldin+github@gmail.com>
2023-05-18 13:25:27 -0400

    feat: initial remix grunge commit

---
+++ app.arc
@@ -0,0 +1,24 @@
+@app
+digitalcanvas-dev-87dc
+
+@http
+/*
+  method any
+  src server
+
+@static
+
+@tables
+user
+  pk *String
+
+password
+  pk *String # userId
+
+note
+  pk *String  # userId
+  sk **String # noteId
+
+@aws
+  region us-east-1
+
---
+++ remix.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
+  future: {
+    v2_errorBoundary: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+  publicPath: "/_static/build/",
+  postcss: true,
+  server: "./server.ts",
+  serverBuildPath: "server/index.js",
+  serverModuleFormat: "cjs",
+  tailwind: true,
+  routes(defineRoutes) {
+    return defineRoutes((route) => {
+      if (process.env.NODE_ENV === "production") return;
+
+      console.log("⚠️  Test routes enabled.");
+
+      const appDir = path.join(__dirname, "app");
+
+      route(
+        "__tests/create-user",
+        path.relative(appDir, "cypress/support/test-routes/create-user.ts")
+      );
+    });
+  },
+};
