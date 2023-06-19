35dacf547c6ebf7b7736571519f5c45bec4cb59b
Simon Goldin <simon.goldin@gmail.com>
2023-06-19 14:58:50 -0400

    chore: remove unused import

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,5 +1,3 @@
-import { faker } from '@faker-js/faker';
-
 describe('smoke tests', () => {
   it('should navigate to contact form when contact buttons are clicked', () => {
     cy.visitAndCheck('/');
