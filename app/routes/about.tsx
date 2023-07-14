a6012d4b6ea5b84fdc9b4612824bc484edd1ca34
Simon Goldin <simon.goldin@gmail.com>
2023-07-14 11:10:58 -0400

    feat: support scrolling to other elements based on hash param
    Signed-off-by: Simon Goldin <simon.goldin@gmail.com>

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -9,7 +9,10 @@ export const About = () => {
   return (
     <IndexSection ref={aboutRef} bgColor="bg-brand/10" border="top">
       <div className="grid w-full grid-flow-row auto-rows-auto items-start justify-stretch gap-14 text-brand md:min-h-[50vh] md:grid-cols-indexLeftHeadingMd">
-        <h3 className="self-start justify-self-start align-top font-heading text-3xl">
+        <h3
+          id="about"
+          className="self-start justify-self-start align-top font-heading text-3xl"
+        >
           About Us
         </h3>
         <div className="text-balance flex flex-col gap-8 self-start justify-self-end font-body font-light text-opacity-80">
